6bd797353696c1b3f8224a39a1201d8d052be722
Celeste Fuentes <celeste.fuentes.g30@gmail.com>
2026-05-16 11:59:50 -0600

    CREACION DE CLASE NODO

---
+++ src/bst/Node.java
@@ -0,0 +1,14 @@
+package bst;
+
+public class Node {
+    int value;
+    Node left;
+    Node right;
+
+    // Constructor = nodo con su valor original
+    public Node(int value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
